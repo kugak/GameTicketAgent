25e4d0add9f1ec214e0a70e0ccc7b18adcd46f49
Kugak <contact@kuga.ca>
2017-09-25 21:40:40 -0400

    linked back button to unwind method

--- GameTicketAgent/ViewControllers/ViewController.swift
+++ GameTicketAgent/ViewControllers/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //back button
+    @IBAction func unwindToThisViewController(segue: UIStoryboardSegue)
+    {
+        
+    }
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
